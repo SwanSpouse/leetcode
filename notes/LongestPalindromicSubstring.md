5b68fae7a693307a3dea77dd684bef90a008d411
李洺吉 <limingji0503@126com>
2017-12-02 16:04:05 +0800

    longest palindromic substring

--- notes/LongestPalindromicSubstring.md
+++ notes/LongestPalindromicSubstring.md
@@ -1,5 +1,6 @@
 
 ### LongestPalindromicSubstring
 
-最开始的想法是分 aba 和 abba 两种类型来处理。 超时
-  
+最开始的想法是分 aba 和 abba 两种类型来处理。 超时   
+
+* Manacher’s Algorithm 要有一定的观察力，这个题和找规律挺像的。 
--- src/main/java/strings/LongestPalindromicSubstring.java
+++ src/main/java/strings/LongestPalindromicSubstring.java
@@ -1,51 +1,61 @@
 package strings;
 
 public class LongestPalindromicSubstring {
+    // Transform S into T.
+    // For example, S = "abba", T = "^#a#b#b#a#$".
+    // ^ and $ signs are sentinels appended to each end to avoid bounds checking
+    public static String preProcess(String s) {
+        int n = s.length();
+        if (n == 0) return "^$";
+
+        String ret = "^";
+        for (int i = 0; i < n; i++) {
+            ret += "#" + s.substring(i, i + 1);
+        }
+
+        ret += "#$";
+        return ret;
+    }
 
     public static String longestPalindrome(String s) {
-        if (s.length() == 0) {
-            return "";
+        String T = preProcess(s);
+        int N = T.length();
+        int[] P = new int[N];
+        int C = 0, R = 0;
+
+        for (int i = 1; i < N - 1; i++) {
+            int mirror = 2 * C - i;
+            /*
+             R > i
+                说明在对称区间内，可以复用左边对称区间的值；
+                P[i] = R -i
+                    是最大对称区间的半径。
+                P[i] = P[mirror]
+                    是对称节点的值
+
+             R <= i
+                说明没有在对称区间内，只能重新进行计算；
+             */
+            P[i] = R > i ? Math.min(R - i, P[mirror]) : 0;
+
+            while (T.charAt(i + 1 + P[i]) == T.charAt(i - 1 - P[i])) {
+                P[i]++;
+            }
+
+            if (i + P[i] > R) {
+                C = i;
+                R = i + P[i];
+            }
         }
         int maxLen = 0;
-        String ret = "";
-        // 找 bab型
-        for (int i = 0; i < s.length(); i++) {
-            String cur = "" + s.charAt(i);
-            int curLen = 1;
-            for (int offset = 1; i + offset < s.length() && i - offset >= 0; offset++) {
-                if (s.charAt(i - offset) != s.charAt(i + offset)) {
-                    break;
-                }
-                cur = s.charAt(i - offset) + cur + s.charAt(i + offset);
-                curLen += 2;
-            }
-            if (curLen > maxLen) {
-                maxLen = curLen;
-                ret = cur;
+        int centerIndex = 0;
+        for (int i = 1; i < N - 1; i++) {
+            if (P[i] > maxLen) {
+                maxLen = P[i];
+                centerIndex = i;
             }
         }
-        if (s.length() == 1) {
-            return ret;
-        }
-        // 找 abba型
-        for (int i = 0, j = i + 1; j < s.length(); i++, j++) {
-            if (s.charAt(i) != s.charAt(j)) {
-                continue;
-            }
-            String cur = "" + s.charAt(i) + s.charAt(j);
-            int curLen = 2;
-            for (int offset = 1; i - offset >= 0 && j + offset < s.length(); offset++) {
-                if (s.charAt(i - offset) != s.charAt(j + offset)) {
-                    break;
-                }
-                cur = s.charAt(i - offset) + cur + s.charAt(j + offset);
-                curLen += 2;
-            }
-            if (curLen > maxLen) {
-                maxLen = curLen;
-                ret = cur;
-            }
-        }
-        return ret;
+        // 这里除以2是从T坐标变换回S坐标用的
+        return s.substring((centerIndex - 1 - maxLen) / 2, (centerIndex - 1 - maxLen) / 2 + maxLen);
     }
 }

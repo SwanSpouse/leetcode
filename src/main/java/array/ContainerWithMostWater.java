73754d541fa4051afcd6bba4940f63021dfaf684
李洺吉 <limingji0503@126com>
2017-12-09 01:03:02 +0800

    Container with most water

--- src/main/java/array/ContainerWithMostWater.java
+++ src/main/java/array/ContainerWithMostWater.java
@@ -2,7 +2,22 @@ package array;
 
 public class ContainerWithMostWater {
     public static int maxArea(int[] height) {
+        int max = 0;
+        for (int i = 0, j = height.length - 1; i <= j; ) {
+            int curArea = (j - i) * Math.min(height[i], height[j]);
+            max = curArea > max ? curArea : max;
+            if (height[i] == Math.min(height[i], height[j])) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return max;
+    }
 
-        return 0;
+
+    public static void main(String[] args) {
+//        System.out.println(maxArea(new int[]{3, 5, 1, 4, 2}));
+        System.out.println(maxArea(new int[]{5, 4, 3, 2, 1}));
     }
 }

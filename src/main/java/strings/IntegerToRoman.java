a5cf58f79d51b5d7a0cfd5d16ac7617d17e70432
李洺吉 <limingji0503@126com>
2017-12-13 00:23:00 +0800

    IntegerToRoman

--- src/main/java/strings/IntegerToRoman.java
+++ src/main/java/strings/IntegerToRoman.java
@@ -1,15 +1,54 @@
 package strings;
 
+import java.util.HashMap;
+
 public class IntegerToRoman {
 
-    public static String appendChar(String input, int nums, char ch) {
-        for(int i=0; i < nums; i ++ ){
+    public static String appendChar(String input, int nums, String ch) {
+        for (int i = 0; i < nums; i++) {
             input += ch;
         }
         return input;
     }
 
     public static String intToRoman(int num) {
-        return "";
+        String roman = "";
+        if (num == 0) {
+            return roman;
+        }
+        HashMap<Integer, String[]> map = new HashMap<Integer, String[]>();
+        map.put(1000, new String[]{"M"});
+        map.put(100, new String[]{"C", "CD", "D", "CM"});
+        map.put(10, new String[]{"X", "XL", "L", "XC"});
+        map.put(1, new String[]{"I", "IV", "V", "IX"});
+
+        for (int divider = 1000; divider > 0; divider /= 10) {
+            int quotient = num / divider;
+            num = num % divider;
+            String[] cur = map.get(divider);
+            if (quotient == 0) {
+                continue;
+            }
+            if (quotient == 1) {
+                roman += cur[0];
+            }
+            if (quotient == 4) {
+                roman += cur[1];
+            }
+            if (quotient == 5) {
+                roman += cur[2];
+            }
+            if (quotient == 9) {
+                roman += cur[3];
+            }
+            if (1 < quotient && quotient < 4) {
+                roman = appendChar(roman, quotient, cur[0]);
+            }
+            if (5 < quotient && quotient < 9) {
+                roman += cur[2];
+                roman = appendChar(roman, quotient - 5, cur[0]);
+            }
+        }
+        return roman;
     }
 }
--- src/test/java/strings/TestIntegerToRoman.java
+++ src/test/java/strings/TestIntegerToRoman.java
@@ -10,21 +10,22 @@ public class TestIntegerToRoman {
 
     @Test
     public void testAppendChar() {
-        assertEquals("aaa", appendChar("", 3, 'a'));
-        assertEquals("aaaaa", appendChar("", 5, 'a'));
-        assertEquals("a", appendChar("", 1, 'a'));
+        assertEquals("aaa", appendChar("", 3, "a"));
+        assertEquals("aaaaa", appendChar("", 5, "a"));
+        assertEquals("a", appendChar("", 1, "a"));
     }
 
     @Test
     public void testIntToRoman(){
         assertEquals("I", intToRoman(1));
-        assertEquals("II", intToRoman(4));
-        assertEquals("IV", intToRoman(5));
+        assertEquals("IV", intToRoman(4));
+        assertEquals("V", intToRoman(5));
         assertEquals("IX", intToRoman(9));
         assertEquals("XIX", intToRoman(19));
         assertEquals("XLVIII", intToRoman(48));
         assertEquals("MMMDCCCLXXI", intToRoman(3871));
         assertEquals("MI", intToRoman(1001));
+        assertEquals("MMMCMXCIX", intToRoman(3999));
     }
 
 }

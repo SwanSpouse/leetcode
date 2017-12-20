d63659eaf7650498ccb0017a5da47031f5f62dfa
李洺吉 <mingjili@creditease.cn>
2017-12-20 18:39:23 +0800

    WIP

--- src/main/java/linkedlist/ReverseNodesInKGroup.java
+++ src/main/java/linkedlist/ReverseNodesInKGroup.java
@@ -1,7 +1,20 @@
 package linkedlist;
 
 class ReverseNodesInKGroup {
-    public ListNode reverseKGroup(ListNode head, int k) {
+
+	public static ListNode reverse(ListNode pre, ListNode tail) {
+		if (pre == NULL || pre.next == NULL) {
+			return pre;
+		}
+		ListNode dummy = new ListNode(-1);
+		dummy.next = pre.next;
+
+		
+
+		return dummy.next;
+	}
+
+    public static ListNode reverseKGroup(ListNode head, int k) {
         
     }	
 }

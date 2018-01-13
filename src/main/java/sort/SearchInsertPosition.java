7ab591d788a992b565f4c98aeaf9f76497a3f98c
李洺吉 <limingji0503@126com>
2018-01-13 14:23:03 +0800

    Search Insert Position

---
+++ src/main/java/sort/SearchInsertPosition.java
@@ -0,0 +1,26 @@
+package sort;
+
+/**
+ *  二分查找的简单变型。
+ *  需要特殊处理的就是如果target大于数组内所有元素的时候。
+ */
+class SearchInsertPosition {
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length -1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] > target) {
+                right = mid - 1;
+            } else if(nums[mid] < target) {
+                left  = mid + 1;
+            }
+        }
+        if (nums.length != 0 && nums[left] < target) {
+            return left + 1;
+        }
+        return left;
+    }
+}

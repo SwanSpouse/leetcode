8b953c4c2f957c08d1d8256826d8fa74d2e8f5e7
李洺吉 <limingji0503@126com>
2018-02-03 12:59:47 +0800

    Sort Colors

---
+++ src/main/java/sort_and_search/SortColors.java
@@ -0,0 +1,36 @@
+package sort_and_search;
+
+import utils.Print;
+
+/**
+ *  技巧性还是很强的。这道题有点儿快排的思想在里面。
+ */
+public class SortColors {
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void sortColors(int[] nums) {
+        int red = 0;
+        int blue = nums.length -1;
+        for(int i=0; i < blue + 1; ) {
+            if(nums[i] == 0) {
+                swap(nums, i ++, red ++);
+            } else if(nums[i] == 2) {
+                swap(nums, i, blue --);
+            } else {
+                i ++ ;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] ret = new int [] {0,1,2,2,1,0};
+        sortColors(ret);
+        Print.PrintArr(ret);
+    }
+
+}

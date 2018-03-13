cb66e2942f5d605f498d9161c6d4290a3202c314
李洺吉 <limingji0503@126com>
2018-03-14 00:04:18 +0800

    Trapping Rain Water

--- src/main/java/array/TrappingRainWater.java
+++ src/main/java/array/TrappingRainWater.java
@@ -1,66 +1,35 @@
 package array;
 
-import utils.Print;
-
-import java.util.ArrayList;
-
 /**
  * 这种数组的题，肯定是从两边往中间靠的方法。不然就超时了。
  */
 class TrappingRainWater {
 
-    public static int getWater(int[] height, int leftPeek, int rightPeek) {
-        int min = height[leftPeek] < height[rightPeek] ? height[leftPeek] : height[rightPeek];
-        int ret = min * (rightPeek - leftPeek - 1);
-        for (int i = leftPeek + 1; i < rightPeek; i++) {
-            if (height[i] > min) {
-                ret = ret - min;
-            } else {
-                ret = ret - height[i];
-            }
-        }
-        System.out.println("left " + leftPeek + " right " + rightPeek + " value " + ret);
-        return ret;
-    }
-
     public static int trap(int[] height) {
-        if (height.length <= 2) {
-            return 0;
+        int[] maxLeft = new int[height.length];
+        int[] maxRight = new int[height.length];
+        int len = height.length;
+
+        for (int i = 1; i < len - 1; i++) {
+            maxLeft[i] = Math.max(height[i - 1], maxLeft[i - 1]);
+            maxRight[len - 1 - i] = Math.max(height[len - i], maxRight[len - i]);
         }
-        ArrayList<Integer> peeks = new ArrayList<Integer>();
-        for (int i = 0; i < height.length; i++) {
-            if (i == 0) {
-                if (height[i] > height[i + 1]) {
-                    peeks.add(i);
-                }
-            } else if (i == height.length - 1) {
-                if (height[i] > height[i - 1]) {
-                    peeks.add(i);
-                }
-            } else if (height[i] > height[i - 1] && height[i] > height[i + 1]) {
-                peeks.add(i);
-            } else if (height[i] == height[i - 1] && height[i] > height[i + 1]) {
-                peeks.add(i);
-            } else if (height[i] > height[i - 1] && height[i] == height[i + 1]) {
-                peeks.add(i);
+
+        int sum = 0;
+        for (int i = 0; i < len; i++) {
+            int curHeight = Math.min(maxLeft[i], maxRight[i]);
+            if (curHeight > height[i]) {
+                sum += curHeight - height[i];
             }
         }
-        Print.PrintList(peeks);
-        if (peeks.size() < 2) {
-            return 0;
-        }
-        int ret = 0;
-        for (int i = 0; i < peeks.size() - 1; i++) {
-            ret += getWater(height, peeks.get(i), peeks.get(i + 1));
-        }
-        return ret;
+        return sum;
     }
 
     public static void main(String[] args) {
-/*        System.out.println(trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+        System.out.println(trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
         System.out.println(trap(new int[]{1, 1, 2, 3, 2, 1, 1}));
         System.out.println(trap(new int[]{3, 3, 2, 1, 2, 3, 1}));
-        System.out.println(trap(new int[]{3, 1, 5, 1, 3}));*/
+        System.out.println(trap(new int[]{3, 1, 5, 1, 3}));
         System.out.println(trap(new int[]{5, 4, 1, 2}));
     }
 }

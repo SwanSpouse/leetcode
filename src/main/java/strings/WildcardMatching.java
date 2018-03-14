3d84e3e28e1b0070ef55a93c755525eb238338f0
李洺吉 <limingji0503@126com>
2018-03-15 00:12:44 +0800

    Wildcard Matching

--- src/main/java/strings/WildcardMatching.java
+++ src/main/java/strings/WildcardMatching.java
@@ -2,38 +2,35 @@ package strings;
 
 public class WildcardMatching {
 
-    public static String simplyPattern(String p) {
-        String ret = "";
-        for (int i = 0; i < p.length(); i++) {
-            if (ret.length() > 0 && ret.charAt(ret.length() - 1) == '*' && p.charAt(i) == '*') {
-                continue;
-            }
-            ret = ret + p.charAt(i);
-        }
-        return ret;
-    }
-
-    public static boolean isMatchSimply(String s, String p) {
-        if (s.equals(p) || (p.length() > 0 && p.replace("*", "").equals(""))) {
-            return true;
-        }
-        if (s.length() == 0) {
-            return false;
-        }
-        if (s.length() != 0 && p.length() == 0) {
-            return false;
-        }
-        if (p.charAt(0) == '*') {
-            return isMatchSimply(s.substring(1, s.length()), p.substring(1, p.length())) || isMatchSimply(s.substring(1, s.length()), p) || isMatchSimply(s, p.substring(1, p.length()));
-        }
-        if (s.charAt(0) == p.charAt(0) || p.charAt(0) == '?') {
-            return isMatchSimply(s.substring(1, s.length()), p.substring(1, p.length()));
-        }
-        return false;
-    }
-
     public static boolean isMatch(String s, String p) {
-        return isMatchSimply(s, simplyPattern(p));
+        boolean hasStar = false;
+        int i = 0, j = 0;
+        for (; i < s.length() && j < p.length(); i++, j++) {
+            if (p.charAt(j) == '?') {
+                continue;
+            } else if (p.charAt(j) == '*') {
+                hasStar = true;
+                while (j < p.length() && p.charAt(j) == '*') {
+                    j++;
+                }
+                if (j == p.length()) {
+                    return true;
+                }
+                i = i - 1;
+                j = j - 1;
+            } else {
+                if (s.charAt(i) != p.charAt(j)) {
+                    if (!hasStar) {
+                        return false;
+                    }
+                    j = j - 1;
+                }
+            }
+        }
+        while (j < p.length() && p.charAt(j) == '*') {
+            j += 1;
+        }
+        return j == p.length() && i == s.length();
     }
 
     public static void main(String[] args) {
@@ -46,8 +43,9 @@ public class WildcardMatching {
 //        System.out.println(isMatch("aab", "c*a*b"));
 //        System.out.println(isMatch("c", "*?*"));
 //        System.out.println(isMatch("ho", "ho**"));
-//        System.out.println(isMatch("babaaababaabababbbbbbaabaabbabababbaababbaaabbbaaab", "***bba**a*bbba**aab**b"));
-        System.out.println(isMatch("bbbbbbbabbaabbabbbbaaabbabbabaaabbababbbabbbabaaabaab", "b*b*ab**ba*b**b***bba"));
+//        System.out.println(isMatch("babaaababaabababbbbbbaabaabbabababbaababbaaabbbaaab", "***bba**a*bbba**aab**b")); //false
+//        System.out.println(isMatch("bbbbbbbabbaabbabbbbaaabbabbabaaabbababbbabbbabaaabaab", "b*b*ab**ba*b**b***bba"));//false
+        System.out.println(isMatch("zacabz", "*a?b*"));
     }
 }
 

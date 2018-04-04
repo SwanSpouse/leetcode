331a23f57fd603af608d9f9d37d903c765323f69
MingjiLi <li_mingji@126.com>
2018-04-04 23:41:03 +0800

    JewelsAndStones

---
+++ src/main/java/detail/JewelsAndStones.java
@@ -0,0 +1,20 @@
+package detail;
+
+import java.util.*;
+
+public class JewelsAndStones {
+    // 打算利用Set
+    public int numJewelsInStones(String J, String S) {
+        HashSet<Character> dict = new HashSet<Character>();
+        for(int i =0; i < J.length(); i ++ ) {
+            dict.add(J.charAt(i));
+        }
+        int count = 0;
+        for(int i=0; i < S.length(); i ++) {
+            if(dict.contains(S.charAt(i))) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+}

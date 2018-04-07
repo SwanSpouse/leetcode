be38e15cfe76fd2e35b9546ab268cd6fb1876e88
MingjiLi <li_mingji@126.com>
2018-04-07 16:37:28 +0800

    Self Dividing Numbers

---
+++ src/main/java/detail/SelfDividingNumbers.java
@@ -0,0 +1,28 @@
+package detail;
+
+import java.util.*;
+
+public class SelfDividingNumbers {
+
+    public boolean isSelfDividingNumber(int input) {
+        int origin  = input;
+        while(input != 0) {
+            int tail = input % 10;
+            if (tail == 0 || origin % tail != 0) {
+                return false;
+            }
+            input = input /10;
+        }
+        return true;
+    }
+
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        ArrayList<Integer> ret= new ArrayList<Integer>();
+        for(int i=left; i <= right; i ++ ) {
+            if(isSelfDividingNumber(i)) {
+                ret.add(i);
+            }
+        }
+        return ret;
+    }
+}

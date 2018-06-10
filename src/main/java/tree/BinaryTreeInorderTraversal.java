f7f12fbf7b740ea1753da836c8d5d3ccb243218a
MingjiLi <li_mingji@126.com>
2018-06-10 21:06:15 +0800

    BinaryTreeInorderTraversal

---
+++ src/main/java/tree/BinaryTreeInorderTraversal.java
@@ -0,0 +1,21 @@
+package tree;
+
+import java.util.*;
+
+public class BinaryTreeInorderTraversal {
+
+	public static void inorderTraversal(List<Integer>ret, TreeNode root) {
+		if (root == null) {
+			return ;
+		}
+		inorderTraversal(ret, root.left);
+		ret.add(root.val);
+		inorderTraversal(ret, root.right);
+	}
+
+	public static List<Integer> inorderTraversal(TreeNode root) {
+		ArrayList<Integer>ret = new ArrayList();
+		inorderTraversal(ret, root);
+		return ret;
+	}
+}

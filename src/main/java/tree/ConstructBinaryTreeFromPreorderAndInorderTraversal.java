2fd11e1bdd0289773e64fe05a09a7ff865a809c3
MingjiLi <li_mingji@126.com>
2018-06-15 22:54:14 +0800

    Construct Binary Tree from Preorder and Inorder Traversal

---
+++ src/main/java/tree/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,25 @@
+package tree;
+
+public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
+	public TreeNode generate(int[] preorder, int[] inorder, int preLeft, int preRight, int inLeft, int inRright) {
+		if(preLeft > preRight) {
+			return null;
+		}
+		int rootVal = preorder[preLeft];
+		TreeNode root = new TreeNode(rootVal);
+		int rootPos = inLeft;
+		for(int i=inLeft; i <= inRright; i ++ ) {
+			if(inorder[i] == rootVal){
+				rootPos = i;
+				break;
+			}
+		}
+		root.left  = generate(preorder, inorder,  preLeft + 1,  preLeft + rootPos - inLeft, inLeft, rootPos -1);
+		root.right = generate(preorder, inorder,  preLeft + rootPos - inLeft + 1, preRight, rootPos +1, inRright);
+		return root;
+	}
+
+	public TreeNode buildTree(int[] preorder, int[] inorder) {
+		return generate(preorder, inorder, 0, preorder.length-1, 0, inorder.length-1);
+	}
+}

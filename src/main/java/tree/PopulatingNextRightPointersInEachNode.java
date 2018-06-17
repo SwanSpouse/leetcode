4fb03e7b47de68e87afa109d1f8a303c64500836
MingjiLi <li_mingji@126.com>
2018-06-17 13:06:35 +0800

    PopulatingNextRightPointersInEachNode

---
+++ src/main/java/tree/PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,22 @@
+package tree;
+
+public class PopulatingNextRightPointersInEachNode {
+
+	public void connectLevel(TreeLinkNode node) {
+		if(node ==null || (node.left == null && node.right ==null)) {
+			return ;
+		}
+		for(TreeLinkNode cur = node; cur != null; cur = cur.next) {
+			cur.left.next = cur.right;
+			if(cur.next != null) {
+				cur.right.next = cur.next.left;
+			}
+		}
+	}
+
+	public void connect(TreeLinkNode root) {
+		for(TreeLinkNode cur = root; cur != null; cur = cur.left) {
+			connectLevel(cur);
+		}
+	}
+}

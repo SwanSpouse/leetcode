744ff854a9be8f57dbdc6387e69043c6b47db859
MingjiLi <li_mingji@126.com>
2018-06-18 16:14:44 +0800

    Triangle

---
+++ src/main/java/dynamic_programing/Triangle.java
@@ -0,0 +1,16 @@
+package dynamic_programing;
+
+import java.util.*;
+
+// dp问题，自下而上 dp[i,j] = min(val[i,j] + min(dp[i+1, j], dp[i+1, j+1])
+public class Triangle {
+	public int minimumTotal(List<List<Integer>> triangle) {
+		for(int row = triangle.size()-2; row >=0; row -= 1) {
+			for(int line = 0; line <= row; line += 1) {
+				int val = triangle.get(row).get(line) + Math.min(triangle.get(row+1).get(line), triangle.get(row+1).get(line+1));
+				triangle.get(row).set(line, val);
+			}
+		}
+		return triangle.get(0).get(0);
+	}
+}

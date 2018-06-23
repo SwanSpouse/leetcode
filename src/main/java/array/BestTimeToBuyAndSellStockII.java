7fe24d7286f9094f659e2925e263e0efd5435276
MingjiLi <li_mingji@126.com>
2018-06-23 11:33:36 +0800

    BestTimeToBuyAndSellStockII

---
+++ src/main/java/array/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,17 @@
+package array;
+
+public class BestTimeToBuyAndSellStockII {
+
+	public int maxProfit(int[] prices) {
+		if(prices.length == 0) {
+			return 0;
+		}
+		int profit;
+		for(int i=1; i < prices.length; i ++ ) {
+			if( prices[i] > prices[i-1]) {
+				profit += prices[i]- prices[i-1];
+			}
+		}
+		return profit;
+    }
+}

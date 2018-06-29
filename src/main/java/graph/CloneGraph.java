05c2ae89ed11389452fb99321bd934551995d423
MingjiLi <li_mingji@126.com>
2018-06-29 22:59:20 +0800

    CloneGraph

--- src/main/java/graph/CloneGraph.java
+++ src/main/java/graph/CloneGraph.java
@@ -1,9 +1,13 @@
 package graph;
 
-
+// TODO
 public class CloneGraph {
 
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
-
+        if (node == null) {
+            return node;
+        }
+        UndirectedGraphNode newNode = new UndirectedGraphNode(node.label);
+        return newNode;
     }
 }

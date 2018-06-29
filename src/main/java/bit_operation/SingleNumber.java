512a4a99041562dba9a8b3e4c18b2058426c842c
MingjiLi <li_mingji@126.com>
2018-06-29 23:02:21 +0800

    Single Number

---
+++ src/main/java/bit_operation/SingleNumber.java
@@ -0,0 +1,13 @@
+package bit_operation;
+
+public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        int ret = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            ret = ret ^ nums[i];
+        }
+        return ret;
+    }
+}

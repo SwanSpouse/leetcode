e84bd0d30cfc0f963256a61e90ec481fe5f24922
MingjiLi <li_mingji@126.com>
2018-06-29 22:26:25 +0800

    PalindromePartitioning

--- src/main/java/dfs/PalindromePartitioning.java
+++ src/main/java/dfs/PalindromePartitioning.java
@@ -5,8 +5,10 @@ import utils.Print;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * dfs变型题。其中判断palindrome的方法还可以继续优化。
+ */
 public class PalindromePartitioning {
-
     public static boolean isPalindrome(String s) {
         for(int i=0; i < s.length()/2; i ++ ) {
             if(s.charAt(i) != s.charAt(s.length()-1-i)) {
@@ -21,14 +23,19 @@ public class PalindromePartitioning {
             ret.add(new ArrayList<String>(cur));
             return ;
         }
-        for(int i=1; i < s.length()-1; i ++ ){
+        for(int i=1; i < s.length(); i ++ ){
             String firstPart = s.substring(0, i);
             if(isPalindrome(firstPart)) {
                 String leftPart = s.substring(i, s.length());
-                cur.add(leftPart);
-                dfs(ret, cur, s);
+                cur.add(firstPart);
+                dfs(ret, cur, leftPart);
                 cur.remove(cur.size()-1);
             }
+        }
+        if(isPalindrome(s)) {
+            cur.add(s);
+            ret.add(new ArrayList<String>(cur));
+            cur.remove(cur.size()-1);
         }
     }
 
@@ -44,7 +51,6 @@ public class PalindromePartitioning {
 
     public static void main(String[] args) {
         List<List<String>> ret = partition("abb");
-
         for(List list : ret) {
             Print.PrintList(list);
         }

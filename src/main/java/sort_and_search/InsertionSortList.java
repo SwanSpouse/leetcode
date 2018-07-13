4e8eebaeb30d4a2f391f459a5f06a5ae41847599
MingjiLi <li_mingji@126.com>
2018-07-13 22:22:07 +0800

    InsertionSortList

--- src/main/java/sort_and_search/InsertionSortList.java
+++ src/main/java/sort_and_search/InsertionSortList.java
@@ -5,27 +5,6 @@ import linkedlist.ListNode;
 
 public class InsertionSortList {
 
-    public ListNode findAndInsert(ListNode head, ListNode preNode, ListNode node) {
-        System.out.println("preNode:" + preNode.val + " node:" + node);
-        ListNode dummy = new ListNode(-1);
-        dummy.next = head;
-        ListNode pre = dummy;
-        ListNode cur = head;
-
-        while (cur != null && cur != node) {
-            if (node.val < cur.val) {
-                preNode.next = node.next;
-                node.next = pre.next;
-                pre.next = node;
-                break;
-            }
-            cur = cur.next;
-            pre = pre.next;
-        }
-        LinkedList.printLinkedList(dummy.next);
-        return dummy.next;
-    }
-
     public ListNode insertionSortList(ListNode head) {
         if (head == null || head.next == null) {
             return head;
@@ -33,14 +12,25 @@ public class InsertionSortList {
         ListNode dummy = new ListNode(-1);
         dummy.next = head;
 
-        ListNode pre = head;
+        ListNode preCur = head;
         ListNode cur = head.next;
         while (cur != null) {
-            ListNode preNext = pre.next;
-            ListNode next = cur.next;
-            dummy.next = findAndInsert(dummy.next, pre, cur);
-            cur = next;
-            pre = preNext;
+            ListNode preNode = dummy;
+            ListNode node = dummy.next;
+            while (node.val < cur.val && node != cur) {
+                preNode = preNode.next;
+                node = node.next;
+            }
+            if (node != cur) {
+                ListNode next = cur.next;
+                preCur.next = cur.next;
+                cur.next = preNode.next;
+                preNode.next = cur;
+                cur = next;
+            } else {
+                preCur = preCur.next;
+                cur = cur.next;
+            }
         }
         return dummy.next;
     }
@@ -48,6 +38,11 @@ public class InsertionSortList {
     public static void main(String[] args) {
         ListNode head = LinkedList.constructLinkedList(new int[]{4, 2, 1, 3});
         ListNode newHead = new InsertionSortList().insertionSortList(head);
-//        LinkedList.printLinkedList(newHead);
+        LinkedList.printLinkedList(newHead);
+
+        head = LinkedList.constructLinkedList(new int[]{-1, 5, 3, 4, 0});
+        newHead = new InsertionSortList().insertionSortList(head);
+        LinkedList.printLinkedList(newHead);
+
     }
 }

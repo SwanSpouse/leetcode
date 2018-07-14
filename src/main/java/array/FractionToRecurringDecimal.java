a7d94fd5c520b3b9e7f47ecda7aa26ae6e6f95b8
MingjiLi <li_mingji@126.com>
2018-07-14 22:49:53 +0800

    FractionToRecurringDecimal

--- src/main/java/array/FractionToRecurringDecimal.java
+++ src/main/java/array/FractionToRecurringDecimal.java
@@ -1,9 +1,6 @@
 package array;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.Map;
 
 public class FractionToRecurringDecimal {
 
@@ -20,57 +17,50 @@ public class FractionToRecurringDecimal {
             }
         } else {
             ret = "0.";
-            numerator = numerator * 10;
         }
         // 处理小数点后的结果
-        int opCount = 0;
-        boolean hasFindRepeatRange = false;
+        int pos = 0;
+        int repeatPos = -1;
         while (numerator != 0) {
             numerator = numerator * 10;
-            opCount += 1;
-
+            pos += 1;
             if (map.containsKey(numerator)) {
-                hasFindRepeatRange = true;
+                repeatPos = map.get(numerator);
                 break;
             } else {
-                map.put(numerator, opCount);
+                map.put(numerator, pos);
             }
 
             while (numerator < denominator) {
                 numerator = numerator * 10;
-                opCount += 1;
+                pos += 1;
                 // 找到重复的被除数就结束了；
                 if (map.containsKey(numerator)) {
-                    hasFindRepeatRange = true;
+                    repeatPos = map.get(numerator);
                     break;
                 } else {
-                    map.put(numerator, opCount);
+                    map.put(numerator, pos);
                 }
                 ret += "0";
             }
 
-            if (hasFindRepeatRange) {
+            if (repeatPos != -1) {
                 break;
             }
             ret += numerator / denominator;
             numerator = numerator % denominator;
         }
 
-        if (hasFindRepeatRange) {
-            ArrayList<Integer> pos = new ArrayList<Integer>();
-            for (Map.Entry<Integer, Integer> e : map.entrySet()) {
-                pos.add(e.getValue());
-            }
-            Collections.sort(pos);
-
+        if (repeatPos != -1) {
             String[] fields = ret.split("\\.");
-            return fields[0] + "." + fields[1].substring(0, pos.get(0) - 1) + "(" + fields[1].substring(pos.get(0) - 1, pos.get(pos.size() - 1) - 1) + ")";
+            return fields[0] + "." + fields[1].substring(0, repeatPos - 1) + "(" + fields[1].substring(repeatPos - 1) + ")";
         } else {
             return ret;
         }
     }
 
     public static void main(String[] args) {
+        System.out.println(fractionToDecimal(1, 6));
         System.out.println(fractionToDecimal(2, 1));
         System.out.println(fractionToDecimal(2, 4));
         System.out.println(fractionToDecimal(100, 13));

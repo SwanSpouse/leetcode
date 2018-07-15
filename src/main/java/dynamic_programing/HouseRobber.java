1f08a24f1f2214f65ab693796ee15d682066c4b1
MingjiLi <li_mingji@126.com>
2018-07-15 17:41:54 +0800

    House Robber

---
+++ src/main/java/dynamic_programing/HouseRobber.java
@@ -0,0 +1,26 @@
+package dynamic_programing;
+
+public class HouseRobber {
+    public int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        } else if (nums.length == 1) {
+            return nums[0];
+        } else if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+        dp[2] = Math.max(dp[0] + nums[2], dp[1]);
+
+        int max = Math.max(dp[1], dp[2]);
+        for (int i = 3; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 3] + nums[i]);
+            if (dp[i] > max) {
+                max = dp[i];
+            }
+        }
+        return max;
+    }
+}

f7b82cbd9667f2eb0bede6cabcc2a2df49900b40
MingjiLi <li_mingji@126.com>
2018-08-04 16:17:04 +0800

    Course Schedule II

--- src/main/java/dfs/CourseScheduleII.java
+++ src/main/java/dfs/CourseScheduleII.java
@@ -3,56 +3,51 @@ package dfs;
 import utils.Print;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 
 public class CourseScheduleII {
 
-    public boolean dfs(HashMap<Integer, HashSet<Integer>> deps, ArrayList<Integer> routine, HashSet<Integer> learned, int numCourse) {
-        if (learned.size() == numCourse) {
-            return true;
-        }
-        for (int i = 0; i < numCourse; i++) {
-            // 没有学过这门课，但是学过了所有这门课的前置课程。
-            if (!learned.contains(i) && (deps.get(i) == null || learned.containsAll(deps.get(i)))) {
-                routine.add(i);
-                learned.add(i);
-                if (dfs(deps, routine, learned, numCourse)) {
-                    return true;
-                }
-                routine.remove(routine.size() - 1);
-                learned.remove(i);
-            }
-        }
-        return false;
-    }
-
+    // 从入度最少的点开始进行遍历。
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        HashMap<Integer, HashSet<Integer>> deps = new HashMap<Integer, HashSet<Integer>>();
+        int[] grap = new int[numCourses];
+
+        ArrayList<Integer> order = new ArrayList<Integer>();
         for (int i = 0; i < prerequisites.length; i++) {
-            if (!deps.containsKey(prerequisites[i][0])) {
-                deps.put(prerequisites[i][0], new HashSet<Integer>());
-            }
-            deps.get(prerequisites[i][0]).add(prerequisites[i][1]);
+            grap[prerequisites[i][0]] += 1;
         }
-        ArrayList<Integer> routine = new ArrayList<Integer>();
-        HashSet<Integer> learned = new HashSet<Integer>();
 
-        dfs(deps, routine, learned, numCourses);
-
-        if (routine.size() == numCourses) {
-            int[] retArr = new int[routine.size()];
-            for (int i = 0; i < routine.size(); i++) {
-                retArr[i] = routine.get(i);
+        for (int j = numCourses - 1; j >= 0; j--) {
+            int del = -1;
+            for (int k = 0; k < numCourses; k++) {
+                if (grap[k] == 0) {
+                    del = k;
+                    grap[k] -= 1;
+                    order.add(k);
+                    break;
+                }
             }
-            return retArr;
+            if (del == -1) {
+                return new int[]{};
+            }
+            for (int i = 0; i < prerequisites.length; i++) {
+                if (prerequisites[i][1] == del) {
+                    grap[prerequisites[i][0]] -= 1;
+                }
+            }
         }
-        return new int[]{};
+
+        int[] retArr = new int[order.size()];
+        for (int i = 0; i < order.size(); i++) {
+            retArr[i] = order.get(i);
+        }
+        return retArr;
     }
 
     public static void main(String[] args) {
         Print.PrintArr(new CourseScheduleII().findOrder(4, new int[][]{{1, 0}, {2, 0}, {3, 1}, {3, 2}}));
         Print.PrintArr(new CourseScheduleII().findOrder(3, new int[][]{{1, 0}, {0, 1}}));
         Print.PrintArr(new CourseScheduleII().findOrder(3, new int[][]{{0, 1}, {0, 2}, {1, 2}}));
+
+        Print.PrintArr(new CourseScheduleII().findOrder(100, new int[][]{{6, 27}, {83, 9}, {10, 95}, {48, 67}, {5, 71}, {18, 72}, {7, 10}, {92, 4}, {68, 84}, {6, 41}, {82, 41}, {18, 54}, {0, 2}, {1, 2}, {8, 65}, {47, 85}, {39, 51}, {13, 78}, {77, 50}, {70, 56}, {5, 61}, {26, 56}, {18, 19}, {35, 49}, {79, 53}, {40, 22}, {8, 19}, {60, 56}, {48, 50}, {20, 70}, {35, 12}, {99, 85}, {12, 75}, {2, 36}, {36, 22}, {21, 15}, {98, 1}, {34, 94}, {25, 41}, {65, 17}, {1, 56}, {43, 96}, {74, 57}, {19, 62}, {62, 78}, {50, 86}, {46, 22}, {10, 13}, {47, 18}, {20, 66}, {83, 66}, {51, 47}, {23, 66}, {87, 42}, {25, 81}, {60, 81}, {25, 93}, {35, 89}, {65, 92}, {87, 39}, {12, 43}, {75, 73}, {28, 96}, {47, 55}, {18, 11}, {29, 58}, {78, 61}, {62, 75}, {60, 77}, {13, 46}, {97, 92}, {4, 64}, {91, 47}, {58, 66}, {72, 74}, {28, 17}, {29, 98}, {53, 66}, {37, 5}, {38, 12}, {44, 98}, {24, 31}, {68, 23}, {86, 52}, {79, 49}, {32, 25}, {90, 18}, {16, 57}, {60, 74}, {81, 73}, {26, 10}, {54, 26}, {57, 58}, {46, 47}, {66, 54}, {52, 25}, {62, 91}, {6, 72}, {81, 72}, {50, 35}, {59, 87}, {21, 3}, {4, 92}, {70, 12}, {48, 4}, {9, 23}, {52, 55}, {43, 59}, {49, 26}, {25, 90}, {52, 0}, {55, 8}, {7, 23}, {97, 41}, {0, 40}, {69, 47}, {73, 68}, {10, 6}, {47, 9}, {64, 24}, {95, 93}, {79, 66}, {77, 21}, {80, 69}, {85, 5}, {24, 48}, {74, 31}, {80, 76}, {81, 27}, {71, 94}, {47, 82}, {3, 24}, {66, 61}, {52, 13}, {18, 38}, {1, 35}, {32, 78}, {7, 58}, {26, 58}, {64, 47}, {60, 6}, {62, 5}, {5, 22}, {60, 54}, {49, 40}, {11, 56}, {19, 85}, {65, 58}, {88, 44}, {86, 58}}));
+
     }
 }

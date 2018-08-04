a57f19aeeb833cfac9f51374351a05ff4d63a89a
MingjiLi <li_mingji@126.com>
2018-08-04 15:30:51 +0800

    CourseScheduleII 超时

--- src/main/java/dfs/CourseScheduleII.java
+++ src/main/java/dfs/CourseScheduleII.java
@@ -8,61 +8,51 @@ import java.util.HashSet;
 
 public class CourseScheduleII {
 
+    public boolean dfs(HashMap<Integer, HashSet<Integer>> deps, ArrayList<Integer> routine, HashSet<Integer> learned, int numCourse) {
+        if (learned.size() == numCourse) {
+            return true;
+        }
+        for (int i = 0; i < numCourse; i++) {
+            // 没有学过这门课，但是学过了所有这门课的前置课程。
+            if (!learned.contains(i) && (deps.get(i) == null || learned.containsAll(deps.get(i)))) {
+                routine.add(i);
+                learned.add(i);
+                if (dfs(deps, routine, learned, numCourse)) {
+                    return true;
+                }
+                routine.remove(routine.size() - 1);
+                learned.remove(i);
+            }
+        }
+        return false;
+    }
 
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         HashMap<Integer, HashSet<Integer>> deps = new HashMap<Integer, HashSet<Integer>>();
         for (int i = 0; i < prerequisites.length; i++) {
-            int from = prerequisites[i][0];
-            int to = prerequisites[i][1];
-            if (!deps.containsKey(from)) {
-                HashSet<Integer> set = new HashSet<Integer>();
-                deps.put(from, set);
+            if (!deps.containsKey(prerequisites[i][0])) {
+                deps.put(prerequisites[i][0], new HashSet<Integer>());
             }
-            deps.get(from).add(to);
+            deps.get(prerequisites[i][0]).add(prerequisites[i][1]);
         }
+        ArrayList<Integer> routine = new ArrayList<Integer>();
+        HashSet<Integer> learned = new HashSet<Integer>();
 
-        for (int startPos = 0; startPos < numCourses; startPos++) {
-            ArrayList<Integer> ret = new ArrayList<Integer>();
-            HashSet<Integer> learned = new HashSet<Integer>();
-            HashSet<Integer> lastLearned;
+        dfs(deps, routine, learned, numCourses);
 
-            do {
-                lastLearned = new HashSet<Integer>(learned);
-                for (int i = startPos, count = 0; count < numCourses; i = (i + 1) % numCourses, count++) {
-                    if (learned.contains(i)) {
-                        continue;
-                    }
-                    boolean containsAllDeps = true;
-                    if (deps.get(i) != null) {
-                        for (Integer dep : deps.get(i)) {
-                            if (!learned.contains(dep)) {
-                                containsAllDeps = false;
-                                continue;
-                            }
-                        }
-                    }
-                    if (containsAllDeps) {
-                        ret.add(i);
-                        learned.add(i);
-                    }
-                }
-
-            } while (!(learned.containsAll(lastLearned) && lastLearned.containsAll(lastLearned)));
-
-            if (learned.size() == numCourses) {
-                int[] retArr = new int[ret.size()];
-                for (int i = 0; i < ret.size(); i++) {
-                    retArr[i] = ret.get(i);
-                }
-                return retArr;
+        if (routine.size() == numCourses) {
+            int[] retArr = new int[routine.size()];
+            for (int i = 0; i < routine.size(); i++) {
+                retArr[i] = routine.get(i);
             }
+            return retArr;
         }
         return new int[]{};
     }
 
     public static void main(String[] args) {
-//        Print.PrintArr(new CourseScheduleII().findOrder(4, new int[][]{{1, 0}, {2, 0}, {3, 1}, {3, 2}}));
-//        Print.PrintArr(new CourseScheduleII().findOrder(3, new int[][]{{1, 0}, {0, 1}}));
+        Print.PrintArr(new CourseScheduleII().findOrder(4, new int[][]{{1, 0}, {2, 0}, {3, 1}, {3, 2}}));
+        Print.PrintArr(new CourseScheduleII().findOrder(3, new int[][]{{1, 0}, {0, 1}}));
         Print.PrintArr(new CourseScheduleII().findOrder(3, new int[][]{{0, 1}, {0, 2}, {1, 2}}));
     }
 }

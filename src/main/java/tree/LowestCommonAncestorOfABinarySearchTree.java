779515a980ff23f4f0411673c5081ac2fee6a2b5
MingjiLi <li_mingji@126.com>
2018-08-25 11:44:04 +0800

    Lowest Common Ancestor of a Binary Search Tree

---
+++ src/main/java/tree/LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,34 @@
+package tree;
+
+public class LowestCommonAncestorOfABinarySearchTree {
+
+    public boolean contains(TreeNode root, TreeNode node) {
+        if (root == null) {
+            return false;
+        }
+        if (root.val == node.val) {
+            return true;
+        } else if (node.val > root.val) {
+            return contains(root.right, node);
+        } else {
+            return contains(root.left, node);
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        boolean leftValid = contains(root.left, p) && contains(root.left, q);
+        boolean rightValid = contains(root.right, p) && contains(root.right, q);
+
+        if (!leftValid && !rightValid) {
+            return root;
+        } else if (leftValid && !rightValid) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (!leftValid && rightValid) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return null;
+    }
+}

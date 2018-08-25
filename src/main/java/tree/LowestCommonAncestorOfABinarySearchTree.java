76abac56941c68aa7d2649f28ecb1b1432b3f8c7
MingjiLi <li_mingji@126.com>
2018-08-25 11:46:48 +0800

    better solution LowestCommonAncestorOfABinarySearchTree.java

--- src/main/java/tree/LowestCommonAncestorOfABinarySearchTree.java
+++ src/main/java/tree/LowestCommonAncestorOfABinarySearchTree.java
@@ -2,6 +2,12 @@ package tree;
 
 public class LowestCommonAncestorOfABinarySearchTree {
 
+//    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//        if(root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+//        if(root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+//        return root;
+//    }
+
     public boolean contains(TreeNode root, TreeNode node) {
         if (root == null) {
             return false;

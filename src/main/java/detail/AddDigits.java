4f556a21061e4670fb9caabdc636b53f27ac2b92
MingjiLi <li_mingji@126.com>
2018-08-31 00:40:09 +0800

    Add Digits

---
+++ src/main/java/detail/AddDigits.java
@@ -0,0 +1,15 @@
+package detail;
+
+public class AddDigits {
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int next = 0;
+            while (num != 0) {
+                next += num % 10;
+                num = num / 10;
+            }
+            num = next;
+        }
+        return num;
+    }
+}

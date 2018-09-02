fa2b8dbf1752e99f30045631a25e56df73129a92
MingjiLi <li_mingji@126.com>
2018-09-02 11:04:13 +0800

    Ugly Number

---
+++ src/main/java/detail/UglyNumber.java
@@ -0,0 +1,17 @@
+package detail;
+
+public class UglyNumber {
+    public boolean isUgly(int num) {
+        if (num == 0) {
+            return false;
+        }
+        int[] factors = new int[]{2, 3, 5};
+
+        for (int i = 0; i < factors.length; i++) {
+            while (num % factors[i] == 0) {
+                num = num / factors[i];
+            }
+        }
+        return num == 1;
+    }
+}

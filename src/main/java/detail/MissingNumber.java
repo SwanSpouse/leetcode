852137f392767365e510cde1bcc9764fc3578761
MingjiLi <li_mingji@126.com>
2018-09-02 12:04:21 +0800

    Missing Number

---
+++ src/main/java/detail/MissingNumber.java
@@ -0,0 +1,11 @@
+package detail;
+
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        return (0 + nums.length) * (nums.length + 1) / 2 - sum;
+    }
+}

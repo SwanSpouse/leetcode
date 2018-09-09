06cbe9124c8a6f838eea2918206414eabe369b42
MingjiLi <li_mingji@126.com>
2018-09-09 15:11:01 +0800

    Move Zeroes

---
+++ src/main/java/detail/MoveZeroes.java
@@ -0,0 +1,23 @@
+package detail;
+
+import utils.Print;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j] == 0) {
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{0, 1, 0, 3, 12};
+//        int[] nums = new int[]{0, 0, 1};
+        new MoveZeroes().moveZeroes(nums);
+        Print.PrintArr(nums);
+    }
+}

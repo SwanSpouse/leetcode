73a13659e8706a4af2516d4dab975c574922e6ce
李洺吉 <mingjili@creditease.cn>
2018-10-01 12:41:07 +0800

    383. Ransom Note

---
+++ src/main/java/detail/RansomNote.java
@@ -0,0 +1,24 @@
+package detail;
+
+import java.util.HashMap;
+
+public class RansomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for (Character c : magazine.toCharArray()) {
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        for (Character c : ransomNote.toCharArray()) {
+            if (map.containsKey(c) && map.get(c) > 0) {
+                map.put(c, map.get(c) - 1);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}

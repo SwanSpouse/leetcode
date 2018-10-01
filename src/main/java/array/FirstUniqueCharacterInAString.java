1df3cd670265cb1250c89723045999d270fd5c3c
李洺吉 <mingjili@creditease.cn>
2018-10-01 12:44:30 +0800

    387. First Unique Character in a String

---
+++ src/main/java/array/FirstUniqueCharacterInAString.java
@@ -0,0 +1,16 @@
+package array;
+
+public class FirstUniqueCharacterInAString {
+    public int firstUniqChar(String s) {
+        int[] letter = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            letter[s.charAt(i) - 'a'] += 1;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (letter[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}

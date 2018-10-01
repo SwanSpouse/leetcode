a312730b07c8452fbd930e58d316af80821102c1
李洺吉 <mingjili@creditease.cn>
2018-10-01 11:01:17 +0800

    342. Power of Four

--- src/main/java/detail/PowerOfThree.java
+++ src/main/java/detail/PowerOfThree.java
@@ -5,12 +5,9 @@ public class PowerOfThree {
         if (n == 0) {
             return false;
         }
-        while (n % 3 ==  0) {
-            if (n == 1) {
-                return true;
-            }
+        while (n % 3 == 0) {
             n = n / 3;
         }
-        return false;
+        return n == 1;
     }
 }

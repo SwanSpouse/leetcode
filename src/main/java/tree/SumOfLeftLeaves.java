f94c7ce5c6e7a8f97293cdbdc075fab841edc192
李洺吉 <mingjili@creditease.cn>
2018-10-03 17:55:40 +0800

    404. Sum of Left Leaves

---
+++ src/main/java/tree/SumOfLeftLeaves.java
@@ -0,0 +1,26 @@
+package tree;
+
+public class SumOfLeftLeaves {
+
+    public void dfs(List<TreeNode>leftLeaves, TreeNode root, boolean isLeft) {
+        if(root == null) {
+            return ;
+        }
+        if(root.left == null && root.right == null && isLeft) {
+            leftLeaves.add(root);
+            return ;
+        }
+        dfs(leftLeaves, root.left, true);
+        dfs(leftLeaves, root.right, false);
+    }
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        List<TreeNode> leftLeaves = new ArrayList<TreeNode>();
+        dfs(leftLeaves, root, false);
+        int sum = 0;
+        for(TreeNode node : leftLeaves) {
+            sum += node.val;
+        }
+        return sum;
+    }
+}

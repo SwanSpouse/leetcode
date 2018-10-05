665c730a2c242f588b16f63dbb498e77abe407fd
李洺吉 <mingjili@creditease.cn>
2018-10-05 13:46:37 +0800

    852. Peak Index in a Mountain Array

--- src/main/java/array/PeakIndexInAMountainArray.java
+++ src/main/java/array/PeakIndexInAMountainArray.java
@@ -2,9 +2,15 @@ package array;
 
 public class PeakIndexInAMountainArray {
     public int peakIndexInMountainArray(int[] A) {
-        for(int i=1; i < A.length; i ++) {
-            if(A[i] < A[i-1]) {
-                return i - 1;
+        int l = 0, r = A.length - 1, mid;
+        while (l < r) {
+            mid = (l + r) / 2;
+            if (A[mid] < A[mid + 1]) {
+                l = mid;
+            } else if (A[mid - 1] > A[mid]) {
+                r = mid;
+            } else {
+                return mid;
             }
         }
         return 0;

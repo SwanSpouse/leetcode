a881d00749ef50bda7649e5f856354bc60f01062
李洺吉 <mingjili@creditease.cn>
2018-10-06 16:56:20 +0800

    669. Trim a Binary Search Tree

---
+++ src/main/java/tree/TrimABinarySearchTree.java
@@ -0,0 +1,18 @@
+package tree;
+
+public class TrimABinarySearchTree {
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if(root == null) {
+            return null;
+        }
+        if(root.val < L ) {
+            return trimBST(root.right, L, R);
+        } else if(root.val > R) {
+            return trimBST(root.left, L, R);
+        } else {
+            root.left = trimBST(root.left, L, R);
+            root.right = trimBST(root.right, L, R);
+            return root;
+        }
+    }
+}

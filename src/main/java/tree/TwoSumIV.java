2e1b15d5a2f6710a7e6ac9f86b11d5764e8d7c18
李洺吉 <mingjili@creditease.cn>
2018-10-06 18:21:00 +0800

    653. Two Sum IV - Input is a BST

---
+++ src/main/java/tree/TwoSumIV.java
@@ -0,0 +1,29 @@
+package tree;
+
+public class TwoSumIV {
+
+    public void inorder(List<Integer> vals, TreeNode root) {
+        if(root == null) {
+            return ;
+        }
+        inorder(vals, root.left);
+        vals.add(root.val);
+        inorder(vals, root.right);
+    }
+
+    public boolean findTarget(TreeNode root, int k) {
+        List<Integer> vals = new ArrayList<Integer>();
+        inorder(vals, root);
+        for(int i=0, j = vals.size()-1; i < j; )  {
+            int cur = vals.get(i) + vals.get(j);
+            if( cur == k ) {
+                return true;
+            } else if(cur > k) {
+                j -= 1;
+            } else {
+                i += 1;
+            }
+        }
+        return false;
+    }
+}

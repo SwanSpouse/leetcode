08a7b970ff0542fe78b5025d0500375771755c30
李洺吉 <mingjili@creditease.cn>
2018-10-07 10:11:23 +0800

    538. Convert BST to Greater Tree

---
+++ src/main/java/tree/ConvertBSTToGreaterTree.java
@@ -0,0 +1,22 @@
+package tree;
+
+public class ConvertBSTToGreaterTree {
+
+    public void inorder(List<TreeNode> nodes, TreeNode root) {
+        if(root == null) {
+            return ;
+        }
+        inorder(nodes, root.left);
+        nodes.add(root);
+        inorder(nodes, root.right);
+    }
+
+    public TreeNode convertBST(TreeNode root) {
+        List<TreeNode> nodes = new ArrayList<TreeNode>();
+        inorder(nodes, root);
+        for(int i =nodes.size()-2; i >= 0; i --) {
+            nodes.get(i).val += nodes.get(i+1).val;
+        }
+        return root;
+    }
+}

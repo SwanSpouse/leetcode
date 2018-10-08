8174a9051931d48e141cf238660f50a9c23ac652
李洺吉 <mingjili@creditease.cn>
2018-10-08 10:29:44 +0800

    628. Maximum Product of Three Numbers

---
+++ src/main/java/array/MaximumProductOfThreeNumbers.java
@@ -0,0 +1,13 @@
+package array;
+
+public class MaximumProductOfThreeNumbers {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int last = nums.length -1;
+        if(nums[0] * nums[1] > nums[last - 1] * nums[last - 2]) {
+            return nums[0] * nums[1] * nums[last];
+        } else {
+            return nums[last] * nums[last - 1] * nums[last - 2];
+        }
+    }
+}

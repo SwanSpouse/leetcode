4fc4576fa9e563e6628decae0e5504e9faa2004f
MingjiLi <li_mingji@126.com>
2018-10-13 00:25:53 +0800

    完全背包

--- src/main/java/dynamic_programing/CompletePackageProblem.java
+++ src/main/java/dynamic_programing/CompletePackageProblem.java
@@ -1,22 +1,23 @@
 package dynamic_programing;
 
-public class PackageProblem01 {
-
-    // 01背包
+public class CompletePackageProblem {
+    // 完全背包 完全背包问题可以转化为01背包问题。
     public int solution(int[] w, int[] v, int W) {
         int[] dp = new int[W + 1];
         for (int i = 0; i < w.length; i++) {
             for (int j = W; j >= w[i]; j--) {
-                dp[j] = Math.max(dp[j], dp[j - w[i]] + v[i]);
+                for (int time = 0; j >= time * w[i]; time += 1) {
+                    dp[j] = Math.max(dp[j], dp[j - time * w[i]] + time * v[i]);
+                }
             }
         }
         return dp[W];
     }
 
     public static void main(String[] args) {
-        int[] w = new int[]{1, 3, 5, 7, 8};
-        int[] c = new int[]{1, 3, 5, 7, 8};
-        int W = 10;
+        int[] w = new int[]{3, 5, 7, 9};
+        int[] c = new int[]{3, 5, 7, 9};
+        int W = 13;
         System.out.println(new PackageProblem01().solution(w, c, W));
     }
 }

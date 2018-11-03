adf1167ff0873fcbfb799670acba1f34f20c8474
MingjiLi <li_mingji@126.com>
2018-11-03 12:19:28 +0800

    324.Wiggle Sort II

---
+++ src/main/java/sort_and_search/WiggleSortII.java
@@ -0,0 +1,29 @@
+package sort_and_search;
+
+import utils.Print;
+
+import java.util.Arrays;
+
+public class WiggleSortII {
+
+    public void wiggleSort(int[] nums) {
+        int[] temp = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(temp);
+
+        int mid = (nums.length + 1) / 2;
+        int j = 0;
+        for (int right = temp.length - 1, left = mid - 1; left >= 0; left--, right--) {
+            nums[j++] = temp[left];
+            if (right >= mid) {
+                nums[j++] = temp[right];
+            }
+        }
+        Print.PrintArr(nums);
+    }
+
+    public static void main(String[] args) {
+        new WiggleSortII().wiggleSort(new int[]{1, 3, 2, 2, 3, 1});
+        new WiggleSortII().wiggleSort(new int[]{1, 5, 1, 1, 6});
+        new WiggleSortII().wiggleSort(new int[]{4, 5, 5, 6});
+    }
+}

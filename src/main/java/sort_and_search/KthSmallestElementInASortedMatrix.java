ecf9d873f91e44253b41f2898128354fdceab306
MingjiLi <li_mingji@126.com>
2018-11-09 00:02:12 +0800

    378. Kth Smallest Element in a Sorted Matrix

--- src/main/java/sort_and_search/KthSmallestElementInASortedMatrix.java
+++ src/main/java/sort_and_search/KthSmallestElementInASortedMatrix.java
@@ -1,17 +1,46 @@
 package sort_and_search;
 
-import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class KthSmallestElementInASortedMatrix {
+//    public int kthSmallest(int[][] matrix, int k) {
+//        int[] list = new int[matrix.length * matrix[0].length];
+//        int index = 0;
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix[i].length; j++) {
+//                list[index++] = matrix[i][j];
+//            }
+//        }
+//        Arrays.sort(list);
+//        return list[k - 1];
+//    }
+
     public int kthSmallest(int[][] matrix, int k) {
-        int[] list = new int[matrix.length * matrix[0].length];
-        int index = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                list[index++] = matrix[i][j];
-            }
+        int n = matrix.length;
+        PriorityQueue<Tuple> pq = new PriorityQueue<Tuple>();
+        for (int j = 0; j <= n - 1; j++) {
+            pq.offer(new Tuple(0, j, matrix[0][j]));
         }
-        Arrays.sort(list);
-        return list[k - 1];
+        for (int i = 0; i < k - 1; i++) {
+            Tuple t = pq.poll();
+            if (t.x == n - 1) continue;
+            pq.offer(new Tuple(t.x + 1, t.y, matrix[t.x + 1][t.y]));
+        }
+        return pq.poll().val;
+    }
+
+    static class Tuple implements Comparable<Tuple> {
+        int x, y, val;
+
+        public Tuple(int x, int y, int val) {
+            this.x = x;
+            this.y = y;
+            this.val = val;
+        }
+
+        @Override
+        public int compareTo(Tuple that) {
+            return this.val - that.val;
+        }
     }
 }

6d9b5aff6f38256be58a8a9a1523914ed5a61d89
MingjiLi <li_mingji@126.com>
2018-11-10 23:37:36 +0800

    386. Lexicographical Numbers WIP

--- src/main/java/sort_and_search/LexicographicalNumbers.java
+++ src/main/java/sort_and_search/LexicographicalNumbers.java
@@ -1,22 +1,33 @@
 package sort_and_search;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class LexicographicalNumbers {
-    public List<Integer> lexicalOrder(int n) {
-        List<Integer> ret = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            ret.add(i);
-        }
-        Collections.sort(ret, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return (o1 + "").compareTo(o2 + "");
+
+    public void generate(List<Integer> ret, int index, int n) {
+        int cur = ret.get(index) * 10;
+        for (int i = 0; i <= 9; i++) {
+            if (cur + i > n) {
+                break;
             }
-        });
+            ret.add(cur + i);
+            generate(ret, index + 1, n);
+        }
+    }
+
+    public List<Integer> lexicalOrder(int n) {
+        ArrayList<Integer> ret = new ArrayList<>();
+        if (n == 0) {
+            return ret;
+        }
+        int index = 0;
+        for (int i = 1; ret.size() < n; i++) {
+            ret.add(i);
+            for (; index < ret.size() && ret.size() < n; index++) {
+                generate(ret, index, n);
+            }
+        }
         return ret;
     }
 

8a5f69bbab2d7e1891046072cbc4dd1c8e12949d
MingjiLi <li_mingji@126.com>
2018-11-10 22:50:11 +0800

    382  Linked List Random Node

--- src/main/java/linkedlist/LinkedListRandomNode.java
+++ src/main/java/linkedlist/LinkedListRandomNode.java
@@ -2,16 +2,37 @@
 
 package linkedlist;
 
+import java.util.Random;
+
 public class LinkedListRandomNode {
 
-    /** @param head The linked list's head.
-        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    /**
+     * @param head The linked list's head.
+     * Note that the head is guaranteed to be not null, so it contains at least one node.
+     */
+
+    ListNode head = null;
+    Random randomGenerator = null;
+
     public LinkedListRandomNode(ListNode head) {
-        
+        this.head = head;
+        this.randomGenerator = new Random();
+
     }
 
-    /** Returns a random node's value. */
+    /**
+     * Returns a random node's value.
+     */
     public int getRandom() {
+        ListNode result = null;
+        ListNode current = head;
 
+        for (int n = 1; current != null; n++) {
+            if (randomGenerator.nextInt(n) == 0) {
+                result = current;
+            }
+            current = current.next;
+        }
+        return result.val;
     }
 }

3c7d3803709636842a031d3c1a5b556fe493cdc3
李洺吉 <mingjili@creditease.cn>
2018-11-10 11:30:42 +0800

    WIP LinkedListRandomNode

---
+++ src/main/java/linkedlist/LinkedListRandomNode.java
@@ -0,0 +1,17 @@
+
+
+package linkedlist;
+
+public class LinkedListRandomNode {
+
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    public LinkedListRandomNode(ListNode head) {
+        
+    }
+
+    /** Returns a random node's value. */
+    public int getRandom() {
+
+    }
+}

72b1d303131b3c322760a96aca6b77d7d5187373
MingjiLi <li_mingji@126.com>
2018-11-11 20:39:47 +0800

    394. Decode String WIP

--- src/main/java/strings/DecodeString.java
+++ src/main/java/strings/DecodeString.java
@@ -1,4 +1,18 @@
 package strings;
 
 public class DecodeString {
+
+    public String decodeString(String s) {
+        return "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new DecodeString().decodeString("3[a]2[bc]"));
+        System.out.println(new DecodeString().decodeString("3[a2[c]]"));
+        System.out.println(new DecodeString().decodeString("2[abc]3[cd]ef"));
+    }
 }
+
+// s = "3[a]2[bc]", return "aaabcbc".
+// s = "3[a2[c]]", return "accaccacc".
+// s = "2[abc]3[cd]ef", return "abcabccdcdcdef".

3ae868e24ad387b6edf215f6e07939a8cf7246e0
MingjiLi <li_mingji@126.com>
2018-11-17 15:52:04 +0800

    385. Mini Parser

--- src/main/java/detail/MiniParser.java
+++ src/main/java/detail/MiniParser.java
@@ -2,37 +2,38 @@ package detail;
 
 public class MiniParser {
 
-    private int index;
-
-    public NestedInteger readNestedInteger(String s) {
-        while (index < s.length() && s.charAt(index) == ',') {
-            index += 1;
-        }
-        if (s.charAt(index) == '[') {
-            index += 1;
-            NestedInteger ni = new NestedInteger();
-            while (index < s.length() && s.charAt(index) != ']') {
-                ni.add(readNestedInteger(s));
-            }
-            index += 1;
-            return ni;
-        } else {
-            String cur = "";
-            while (index < s.length() && s.charAt(index) != ',' && s.charAt(index) != ']') {
-                cur += s.charAt(index++);
-            }
-            if (index < s.length() && s.charAt(index) != ']') {
-                index += 1;
-            }
-            if (cur.length() == 0) {
-                return new NestedInteger();
-            }
-            return new NestedInteger(Integer.parseInt(cur));
-        }
-    }
-
-    public NestedInteger deserialize(String s) {
-        this.index = 0;
-        return readNestedInteger(s);
-    }
+// 为了不编译报错。把下面的解题代码注释掉了。
+//    private int index;
+//
+//    public NestedInteger readNestedInteger(String s) {
+//        while (index < s.length() && s.charAt(index) == ',') {
+//            index += 1;
+//        }
+//        if (s.charAt(index) == '[') {
+//            index += 1;
+//            NestedInteger ni = new NestedInteger();
+//            while (index < s.length() && s.charAt(index) != ']') {
+//                ni.add(readNestedInteger(s));
+//            }
+//            index += 1;
+//            return ni;
+//        } else {
+//            String cur = "";
+//            while (index < s.length() && s.charAt(index) != ',' && s.charAt(index) != ']') {
+//                cur += s.charAt(index++);
+//            }
+//            if (index < s.length() && s.charAt(index) != ']') {
+//                index += 1;
+//            }
+//            if (cur.length() == 0) {
+//                return new NestedInteger();
+//            }
+//            return new NestedInteger(Integer.parseInt(cur));
+//        }
+//    }
+//
+//    public NestedInteger deserialize(String s) {
+//        this.index = 0;
+//        return readNestedInteger(s);
+//    }
 }

c9603dad979c5f90bf4fef05d2ff1f248ded6414
MingjiLi <li_mingji@126.com>
2018-11-29 23:49:38 +0800

    665. Non-decreasing Array

--- src/main/java/array/NonDecreasingArray.java
+++ src/main/java/array/NonDecreasingArray.java
@@ -2,23 +2,23 @@ package array;
 
 public class NonDecreasingArray {
     public boolean checkPossibility(int[] nums) {
-        if (nums.length == 0) {
-            return true;
-        }
-        int recorderCount = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
-                if (i + 1 < nums.length && i - 1 >= 0 && nums[i + 1] - nums[i - 1] == 1) {
-                    return false;
+        int cnt = 0;
+        for (int i = 1; i < nums.length && cnt <= 1; i++) {
+            if (nums[i - 1] > nums[i]) {
+                cnt++;
+                if (i - 2 < 0 || nums[i - 2] <= nums[i]) {
+                    nums[i - 1] = nums[i];  //modify nums[i-1] of a priority
+                } else {
+                    nums[i] = nums[i - 1];     //have to modify nums[i]
                 }
-                recorderCount += 1;
             }
         }
-        return recorderCount <= 1;
+        return cnt <= 1;
     }
 
     public static void main(String[] args) {
         System.out.println(new NonDecreasingArray().checkPossibility(new int[]{4, 2, 3}));
         System.out.println(new NonDecreasingArray().checkPossibility(new int[]{2, 4, 3}));
+        System.out.println(new NonDecreasingArray().checkPossibility(new int[]{3, 4, 2, 3}));
     }
 }

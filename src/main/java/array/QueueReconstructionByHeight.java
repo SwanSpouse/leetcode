957d6e62d46ef4ab30169f1a3b44f28b81915955
LiMingji <limingji0503@126.com>
2018-12-08 00:02:01 +0800

    406. Queue Reconstruction by Height

--- src/main/java/array/QueueReconstructionByHeight.java
+++ src/main/java/array/QueueReconstructionByHeight.java
@@ -2,74 +2,34 @@ package array;
 
 import utils.Print;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.LinkedList;
-import java.util.List;
 
 public class QueueReconstructionByHeight {
 
-    public static class Person {
-        int height;
-        int count;
-
-        Person(int height, int count) {
-            this.height = height;
-            this.count = count;
-        }
-
-        @Override
-        public String toString() {
-            return "{" + height + "," + count + "}";
-        }
-    }
-
     public int[][] reconstructQueue(int[][] people) {
-        if (people.length == 0) {
+        if (people.length == 0)
             return people;
-        }
-        List<Person> persons = new LinkedList<Person>();
-        for (int i = 0; i < people.length; i++) {
-            persons.add(new Person(people[i][0], people[i][1]));
-        }
 
-        Collections.sort(persons, new Comparator<Person>() {
-            @Override
-            public int compare(Person o1, Person o2) {
-                if (o1.count != o2.count) {
-                    return o1.count - o2.count;
-                }
-                return o1.height - o2.height;
+        Arrays.sort(people, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                if (b[0] == a[0]) return a[1] - b[1];
+                return b[0] - a[0];
             }
         });
 
-        List<Person> ret = new LinkedList<>();
-        for (int i = 0; i < persons.size(); i++) {
-            if (i == 0 || persons.get(i).count == 0) {
-                ret.add(persons.get(i));
-                continue;
-            }
-            Person p = persons.get(i);
-            int count = 0;
-            int curIndex = 0;
-            while (curIndex < ret.size() && count <= p.count) {
-                if (ret.get(curIndex).height >= p.height) {
-                    if (count == p.count && ret.get(curIndex).height >= p.height) {
-                        break;
-                    }
-                    count += 1;
-                }
-                curIndex += 1;
-            }
-            ret.add(curIndex, p);
+        LinkedList<int[]> list = new LinkedList<>();
+        for (int i = 0; i < people.length; i++) {
+            // 它前面都是比它大的，所以直接按照它的count来放就完了。
+            list.add(people[i][1], new int[]{people[i][0], people[i][1]});
         }
 
-        int[][] retArr = new int[people.length][people[0].length];
-        for (int i = 0; i < ret.size(); i++) {
-            retArr[i][0] = ret.get(i).height;
-            retArr[i][1] = ret.get(i).count;
+        for (int i = 0; i < list.size(); i++) {
+            people[i][0] = list.get(i)[0];
+            people[i][1] = list.get(i)[1];
         }
-        return retArr;
+        return people;
     }
 
     public static void main(String[] args) {

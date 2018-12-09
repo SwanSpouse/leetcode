6d263b46335291c60e096e94afb9b46f9d37370c
LiMingji <limingji0503@126.com>
2018-12-09 15:14:15 +0800

    423. Reconstruct Original Digits from English

--- src/main/java/dfs/ReconstructOriginalDigitsFromEnglish.java
+++ src/main/java/dfs/ReconstructOriginalDigitsFromEnglish.java
@@ -1,76 +1,32 @@
 package dfs;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-
 public class ReconstructOriginalDigitsFromEnglish {
 
-    HashMap<Integer, int[]> map;
-
-    public void initMap() {
-        map = new HashMap<>();
-        String[] all = new String[]{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        for (int i = 0; i < all.length; i++) {
-            int[] cur = new int[26];
-            for (int index = 0; index < all[i].length(); index++) {
-                cur[all[i].charAt(index) - 'a'] += 1;
-            }
-            map.put(i, cur);
-        }
-    }
-
-    public boolean isEmpty(int[] input) {
-        for (int i = 0; i < input.length; i++) {
-            if (input[i] != 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public boolean sub(int[] input, int[] num) {
-        for (int i = 0; i < input.length; i++) {
-            if (input[i] < num[i]) {
-                return false;
-            }
-            input[i] = input[i] - num[i];
-        }
-        return true;
-    }
-
-    public void dfs(int[] input, int cur, ArrayList<Integer> ret, ArrayList<Integer> curArr) {
-        if (ret.size() != 0) {
-            return;
-        }
-        if (isEmpty(input)) {
-            ret.addAll(curArr);
-            return;
-        }
-        for (int i = cur; i < 10; i++) {
-            int[] copy = Arrays.copyOfRange(input, 0, input.length);
-            if (sub(copy, map.get(i))) {
-                curArr.add(i);
-                dfs(copy, i, ret, curArr);
-                curArr.remove(curArr.size() - 1);
-            } else {
-                dfs(input, i + 1, ret, curArr);
-            }
-        }
-    }
-
     public String originalDigits(String s) {
-        initMap();
-        int[] sArr = new int[26];
+        int[] count = new int[10];
         for (int i = 0; i < s.length(); i++) {
-            sArr[s.charAt(i) - 'a'] += 1;
+            char c = s.charAt(i);
+            if (c == 'z') count[0]++;
+            if (c == 'w') count[2]++;
+            if (c == 'x') count[6]++;
+            if (c == 's') count[7]++; //7-6
+            if (c == 'g') count[8]++;
+            if (c == 'u') count[4]++;
+            if (c == 'f') count[5]++; //5-4
+            if (c == 'h') count[3]++; //3-8
+            if (c == 'i') count[9]++; //9-8-5-6
+            if (c == 'o') count[1]++; //1-0-2-4
         }
-        ArrayList<Integer> ret = new ArrayList<>();
-        dfs(sArr, 0, ret, new ArrayList<>());
-
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < ret.size(); i++) {
-            sb.append(ret.get(i));
+        count[7] -= count[6];
+        count[5] -= count[4];
+        count[3] -= count[8];
+        count[9] = count[9] - count[8] - count[5] - count[6];
+        count[1] = count[1] - count[0] - count[2] - count[4];
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= 9; i++) {
+            for (int j = 0; j < count[i]; j++) {
+                sb.append(i);
+            }
         }
         return sb.toString();
     }

5cab1db57ab3e7b2016e04075784bc66092fefca
LiMingji <limingji0503@126.com>
2018-12-16 13:16:56 +0800

    WIP 450. Delete Node in a BST

---
+++ src/main/java/tree/DeleteNodeInABST.java
@@ -0,0 +1,11 @@
+package tree;
+
+public class DeleteNodeInABST {
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        return root;
+    }
+}

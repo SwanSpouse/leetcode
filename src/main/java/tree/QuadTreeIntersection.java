3d121969edfafb200bb0a06bab6e9dc725b907b4
LiMingji <limingji0503@126.com>
2018-12-23 16:06:52 +0800

    558. Quad Tree Intersection

--- src/main/java/tree/QuadTreeIntersection.java
+++ src/main/java/tree/QuadTreeIntersection.java
@@ -22,62 +22,25 @@ public class QuadTreeIntersection {
         }
     }
 
-    public Node intersect(Node quadTree1, boolean val1, Node quadTree2, boolean val2) {
-        Node newNode = new Node();
-        if (quadTree1 == null && quadTree2 == null) {
-            newNode.val = val1 || val2;
-            newNode.isLeaf = true;
-            return newNode;
-        } else if (quadTree1 == null) {
-            newNode.val = val1 || quadTree2.val;
-
-            newNode.topLeft = intersect(null, val1, quadTree2.topLeft, quadTree2.val);
-            newNode.topRight = intersect(null, val1, quadTree2.topRight, quadTree2.val);
-            newNode.bottomLeft = intersect(null, val1, quadTree2.bottomLeft, quadTree2.val);
-            newNode.bottomRight = intersect(null, val1, quadTree2.bottomRight, quadTree2.val);
-        } else if (quadTree2 == null) {
-            newNode.val = val2 || quadTree1.val;
-
-            newNode.topLeft = intersect(null, val2, quadTree1.topLeft, quadTree1.val);
-            newNode.topRight = intersect(null, val2, quadTree1.topRight, quadTree1.val);
-            newNode.bottomLeft = intersect(null, val2, quadTree1.bottomLeft, quadTree1.val);
-            newNode.bottomRight = intersect(null, val2, quadTree1.bottomRight, quadTree1.val);
-        } else {
-            newNode.val = quadTree1.val || quadTree2.val;
-
-            newNode.topLeft = intersect(quadTree1.topLeft, val1, quadTree2.topLeft, quadTree2.val);
-            newNode.topRight = intersect(quadTree1.topRight, val1, quadTree2.topRight, quadTree2.val);
-            newNode.bottomLeft = intersect(quadTree1.bottomLeft, val1, quadTree2.bottomLeft, quadTree2.val);
-            newNode.bottomRight = intersect(quadTree1.bottomRight, val1, quadTree2.bottomRight, quadTree2.val);
-        }
-        newNode.isLeaf = newNode.topLeft == null && newNode.topRight == null && newNode.bottomLeft == null && newNode.bottomRight == null;
-
-        return newNode;
-    }
-
     public Node intersect(Node quadTree1, Node quadTree2) {
-        if (quadTree1 == null) {
-            return quadTree2;
-        } else if (quadTree2 == null) {
-            return quadTree1;
+        if (quadTree1.isLeaf) {
+            return quadTree1.val ? quadTree1 : quadTree2;
+        } else if (quadTree2.isLeaf) {
+            return quadTree2.val ? quadTree2 : quadTree1;
         }
+
         Node newNode = new Node();
-        if ((quadTree1.isLeaf && quadTree1.val) || (quadTree2.isLeaf && quadTree2.val)) {
+        newNode.topLeft = intersect(quadTree1.topLeft, quadTree2.topLeft);
+        newNode.topRight = intersect(quadTree1.topRight, quadTree2.topRight);
+        newNode.bottomLeft = intersect(quadTree1.bottomLeft, quadTree2.bottomLeft);
+        newNode.bottomRight = intersect(quadTree1.bottomRight, quadTree2.bottomRight);
+
+        boolean cur = newNode.topLeft.val;
+        if (cur == newNode.topRight.val && cur == newNode.bottomLeft.val && cur == newNode.bottomRight.val &&
+                newNode.topRight.isLeaf && newNode.topRight.isLeaf && newNode.bottomLeft.isLeaf && newNode.bottomRight.isLeaf) {
             newNode.isLeaf = true;
-            newNode.val = true;
-            return newNode;
-        } else if (quadTree1.isLeaf && !quadTree1.val && quadTree2.isLeaf && !quadTree2.val) {
-            newNode.isLeaf = true;
-            newNode.val = false;
-            return newNode;
+            newNode.val = cur;
         }
-
-        newNode.topLeft = intersect(quadTree1.topLeft, quadTree1.val, quadTree2.topLeft, quadTree2.val);
-        newNode.topRight = intersect(quadTree1.topRight, quadTree1.val, quadTree2.topRight, quadTree2.val);
-        newNode.bottomLeft = intersect(quadTree1.bottomLeft, quadTree1.val, quadTree2.bottomLeft, quadTree2.val);
-        newNode.bottomRight = intersect(quadTree1.bottomRight, quadTree1.val, quadTree2.bottomRight, quadTree2.val);
-
-        newNode.isLeaf = newNode.topLeft == null && newNode.topRight == null && newNode.bottomLeft == null && newNode.bottomRight == null;
         return newNode;
     }
 }

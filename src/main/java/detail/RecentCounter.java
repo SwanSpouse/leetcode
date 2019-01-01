6f54a0f489e0c1d6edf90825394e91cc660de015
LiMingji <limingji0503@126.com>
2019-01-01 12:43:11 +0800

    933. Number of Recent Calls

--- src/main/java/detail/RecentCounter.java
+++ src/main/java/detail/RecentCounter.java
@@ -1,41 +1,20 @@
 package detail;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class RecentCounter {
 
-    List<Integer> pings;
-
-    private int binarySearch(int value) {
-        int left = 0;
-        int right = pings.size() - 1;
-        int ret = -1;
-        while (left <= right) {
-            int min = (right - left) / 2 + left;
-            if (pings.get(min) < value) {
-                left = min + 1;
-            } else if (pings.get(min) >= value) {
-                if (pings.get(min) == value) {
-                    ret = min;
-                }
-                right = min - 1;
-            }
-        }
-        return ret != -1 ? ret : left;
-    }
+    Deque<Integer> deque;
 
     public RecentCounter() {
-        pings = new ArrayList<>();
+        deque = new LinkedList<>();
     }
 
     public int ping(int t) {
-        pings.add(t);
-        int left = binarySearch(t - 3000);
-        if (pings.get(left) < t - 3000) {
-            left += 1;
-        }
-        pings = pings.subList(left, pings.size());
-        return pings.size();
+        deque.offerLast(t);
+        while (!deque.isEmpty() && deque.getFirst() < (t - 3000)) deque.pollFirst();
+
+        return deque.size();
     }
 }

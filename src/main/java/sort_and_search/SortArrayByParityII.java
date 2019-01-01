7ff93c7331d1e5e185567171e93a4b157f3173da
LiMingji <limingji0503@126.com>
2019-01-01 13:15:02 +0800

    922. Sort Array By Parity II

--- src/main/java/sort_and_search/SortArrayByParityII.java
+++ src/main/java/sort_and_search/SortArrayByParityII.java
@@ -1,27 +1,31 @@
 package sort_and_search;
 
-import java.util.ArrayList;
+import utils.Print;
 
 public class SortArrayByParityII {
     public int[] sortArrayByParityII(int[] A) {
-        ArrayList<Integer> odds = new ArrayList<>();
-        ArrayList<Integer> evens = new ArrayList<>();
-        for (int i = 0; i < A.length; i++) {
-            if (A[i] % 2 == 0) {
-                evens.add(A[i]);
-            } else {
-                odds.add(A[i]);
+        int i = 0, j = 1, n = A.length;
+        while (i < n && j < n) {
+            while (i < n && A[i] % 2 == 0) {
+                i += 2;
+            }
+            while (j < n && A[j] % 2 == 1) {
+                j += 2;
+            }
+            if (i < n && j < n) {
+                swap(A, i, j);
             }
         }
-        int oddIndex = 0, evenIndex = 0;
-        int[] ret = new int[A.length];
-        for (int i = 0; i < ret.length; i++) {
-            if (i % 2 == 0) {
-                ret[i] = evens.get(evenIndex++);
-            } else {
-                ret[i] = odds.get(oddIndex++);
-            }
-        }
-        return ret;
+        return A;
+    }
+
+    private void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Print.printArr(new SortArrayByParityII().sortArrayByParityII(new int[]{2, 4, 1, 1, 1, 3, 4, 3, 2, 0}));
     }
 }

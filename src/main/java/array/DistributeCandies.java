4fb4baaf2e1063567fde34c842b2c7cbcafe483e
LiMingji <limingji0503@126.com>
2019-01-05 20:54:04 +0800

    575. Distribute Candies

--- src/main/java/array/DistributeCandies.java
+++ src/main/java/array/DistributeCandies.java
@@ -3,7 +3,9 @@ package array;
 import java.util.HashSet;
 
 public class DistributeCandies {
+
     public int distributeCandies(int[] candies) {
+        // 所有能用set的地方，如果限制了范围，就可以用int[]来替代set，这样可以速度更快
         HashSet<Integer> kinds = new HashSet<>();
         for (int i = 0; i < candies.length; i++) {
             kinds.add(candies[i]);

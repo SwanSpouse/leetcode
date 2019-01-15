e13f367d1c92e419ab5c8b38342dfeff6cd54bcb
LiMingji <limingji0503@126.com>
2019-01-16 00:11:54 +0800

    BinarySearch

--- src/main/java/sort_and_search/BinarySearch.java
+++ src/main/java/sort_and_search/BinarySearch.java
@@ -1,19 +1,21 @@
 package sort_and_search;
 
+import utils.Print;
+
 public class BinarySearch {
 
     // 普通的二分查找，找到则返回index，否则返回-1
     public int binarySearch(int[] input, int target) {
         int left = 0;
-        int right = input.length-1;
+        int right = input.length - 1;
 
-        while(left <= right) {
+        while (left <= right) {
             int mid = (right - left) / 2 + left;
-            if(input[mid] == target) {
+            if (input[mid] == target) {
                 return mid;
-            } else if(input[mid] > target) {
+            } else if (input[mid] > target) {
                 right = mid - 1;
-            } else if(input[mid] < target) {
+            } else if (input[mid] < target) {
                 left = mid + 1;
             }
         }
@@ -23,18 +25,18 @@ public class BinarySearch {
     // 找最左 如果有重复元素返回最小的index，如果没有重复元素返回 -1
     public int binarySearchLeftMax(int[] input, int target) {
         int left = 0;
-        int right = input.length-1;
+        int right = input.length - 1;
         int ret = -1;
 
-        while(left <= right) {
+        while (left <= right) {
             int mid = (right - left) / 2 + left;
-            if(input[mid] <=  target) {
-                right = mid -1;
-                if(input[mid] == target) {
+            if (input[mid] <= target) {
+                if (input[mid] == target) {
                     ret = mid;
                 }
-            } else if(input[mid] > target) {
                 left = mid + 1;
+            } else if (input[mid] > target) {
+                right = mid - 1;
             }
         }
         return ret;
@@ -46,15 +48,15 @@ public class BinarySearch {
         int right = input.length - 1;
         int ret = -1;
 
-        while(left <= right) {
+        while (left <= right) {
             int mid = (right - left) / 2 + left;
-            if(input[mid] < target) {
-                right = mid -1;
-            } else if(input[mid] >= target) {
-                if(input[mid] == target) {
+            if (input[mid] < target) {
+                left = mid + 1;
+            } else if (input[mid] >= target) {
+                if (input[mid] == target) {
                     ret = mid;
                 }
-                left = mid + 1;
+                right = mid - 1;
             }
         }
         return ret;
@@ -62,10 +64,13 @@ public class BinarySearch {
 
     // 找区间 如果有重复元素，返回重复元素的区间 左右 index
     public int[] binarySearchRange(int[] input, int target) {
-
+        return new int[]{binarySearchRightMin(input, target), binarySearchLeftMax(input, target)};
     }
 
     public static void main(String[] args) {
-        System.out.println(new BinarySearch().binarySearch(new int[]{1,2,3,4,5}, 3));
+        System.out.println(new BinarySearch().binarySearch(new int[]{1, 2, 3, 4, 5}, 3));
+        System.out.println(new BinarySearch().binarySearchLeftMax(new int[]{1, 2, 3, 3, 3, 3, 4, 5}, 3));
+        System.out.println(new BinarySearch().binarySearchRightMin(new int[]{1, 2, 3, 3, 3, 3, 4, 5}, 3));
+        Print.printArr(new BinarySearch().binarySearchRange(new int[]{1, 2, 3, 3, 3, 3, 4, 5}, 3));
     }
 }

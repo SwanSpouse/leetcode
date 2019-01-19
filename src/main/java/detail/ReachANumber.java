042ebd41fd46ef5aa186092fec5071705893eee0
LiMingji <limingji0503@126.com>
2019-01-20 00:34:14 +0800

    754. Reach a Number

--- src/main/java/detail/ReachANumber.java
+++ src/main/java/detail/ReachANumber.java
@@ -1,27 +1,20 @@
 package detail;
 
-import java.util.ArrayList;
-
 public class ReachANumber {
 
     public int reachNumber(int target) {
-        int step = 1;
-
-        ArrayList<Integer> cur;
-        ArrayList<Integer> next = new ArrayList<>();
-        next.add(0);
-        while (true) {
-            cur = new ArrayList<>(next);
-            next.clear();
-            for (int i = 0; i < cur.size(); i++) {
-                if (cur.get(i) + step == target || cur.get(i) - step == target) {
-                    return step;
-                }
-                next.add(cur.get(i) + step);
-                next.add(cur.get(i) - step);
-            }
-            step += 1;
+        target = Math.abs(target);
+        int step = 0;
+        int sum = 0;
+        while (sum < target) {
+            step++;
+            sum += step;
         }
+        while ((sum - target) % 2 != 0) {
+            step++;
+            sum += step;
+        }
+        return step;
     }
 
     public static void main(String[] args) {

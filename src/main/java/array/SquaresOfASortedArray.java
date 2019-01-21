e40ed3e1110606aebde377fc55ac14d690ed50d1
LiMingji <limingji0503@126.com>
2019-01-22 00:03:05 +0800

    977. Squares of a Sorted Array

---
+++ src/main/java/array/SquaresOfASortedArray.java
@@ -0,0 +1,13 @@
+package array;
+
+import java.util.Arrays;
+
+public class SquaresOfASortedArray {
+    public int[] sortedSquares(int[] A) {
+        for (int i = 0; i < A.length; i++) {
+            A[i] = A[i] * A[i];
+        }
+        Arrays.sort(A);
+        return A;
+    }
+}

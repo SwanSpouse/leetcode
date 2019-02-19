0b0d8f31931f5add0b19e0781bd7e5c19d8edd3c
LiMingji <limingji0503@126.com>
2019-02-19 17:43:04 +0800

    530. Minimum Absolute Difference in BST

---
+++ src/main/java/tree/MinimumAbsoluteDifferenceInBST.java
@@ -0,0 +1,27 @@
+package tree;
+
+public class MinimumAbsoluteDifferenceInBST {
+
+    public void inorderTravel(TreeNode root, ArrayList<Integer> ret) {
+        if(root == null) {
+            return ;
+        }
+        inorderTravel(root.left, ret);
+        ret.add(root.val);
+        inorderTravel(root.right, ret);
+    }
+
+    public int getMinimumDifference(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        ArrayList<Integer> order = new ArrayList<>();
+        inorderTravel(root, order);
+
+        int minDifference = Integer.MAX_VALUE;
+        for(int i=0; i < order.size()-1; i ++ ) {
+            minDifference = Math.min(minDifference, order.get(i+1) - order.get(i));
+        }
+        return minDifference;
+    }
+}

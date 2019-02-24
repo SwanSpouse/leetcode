a2983a8090933dbb6c444a7d703af42909e006e1
LiMingji <limingji0503@126.com>
2019-02-24 11:43:42 +0800

    format codes

--- src/main/java/array/MinimumIndexSumOfTwoLists.java
+++ src/main/java/array/MinimumIndexSumOfTwoLists.java
@@ -1,29 +1,32 @@
 package array;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MinimumIndexSumOfTwoLists {
     public String[] findRestaurant(String[] list1, String[] list2) {
         HashMap<String, Integer> index = new HashMap<>();
-        for(int i =0; i < list1.length; i ++ ) {
+        for (int i = 0; i < list1.length; i++) {
             index.put(list1[i], i);
         }
 
         HashMap<String, Integer> common = new HashMap<>();
         int minIndex = Integer.MAX_VALUE;
-        for(int i =0; i < list2.length; i ++ ) {
-            if(index.containsKey(list2[i]) && index.get(list2[i]) + i <= minIndex) {
+        for (int i = 0; i < list2.length; i++) {
+            if (index.containsKey(list2[i]) && index.get(list2[i]) + i <= minIndex) {
                 minIndex = index.get(list2[i]) + i;
                 common.put(list2[i], minIndex);
             }
         }
 
         ArrayList<String> ret = new ArrayList<>();
-        for(String key : common.keySet()) {
-            if(common.get(key) == minIndex) {
+        for (String key : common.keySet()) {
+            if (common.get(key) == minIndex) {
                 ret.add(key);
             }
         }
         String[] r = new String[ret.size()];
-        for(int i =0; i < ret.size(); i ++ ) {
+        for (int i = 0; i < ret.size(); i++) {
             r[i] = ret.get(i);
         }
         return r;
--- src/main/java/tree/MinimumAbsoluteDifferenceInBST.java
+++ src/main/java/tree/MinimumAbsoluteDifferenceInBST.java
@@ -1,10 +1,12 @@
 package tree;
 
+import java.util.ArrayList;
+
 public class MinimumAbsoluteDifferenceInBST {
 
     public void inorderTravel(TreeNode root, ArrayList<Integer> ret) {
-        if(root == null) {
-            return ;
+        if (root == null) {
+            return;
         }
         inorderTravel(root.left, ret);
         ret.add(root.val);
@@ -12,15 +14,15 @@ public class MinimumAbsoluteDifferenceInBST {
     }
 
     public int getMinimumDifference(TreeNode root) {
-        if(root == null) {
+        if (root == null) {
             return 0;
         }
         ArrayList<Integer> order = new ArrayList<>();
         inorderTravel(root, order);
 
         int minDifference = Integer.MAX_VALUE;
-        for(int i=0; i < order.size()-1; i ++ ) {
-            minDifference = Math.min(minDifference, order.get(i+1) - order.get(i));
+        for (int i = 0; i < order.size() - 1; i++) {
+            minDifference = Math.min(minDifference, order.get(i + 1) - order.get(i));
         }
         return minDifference;
     }

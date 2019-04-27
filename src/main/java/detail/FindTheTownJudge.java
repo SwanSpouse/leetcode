2bf0d54584d63a0467b785c475b7a60696a7715c
LiMingji <limingji0503@126.com>
2019-04-27 16:06:56 +0800

    997. Find the Town Judge

---
+++ src/main/java/detail/FindTheTownJudge.java
@@ -0,0 +1,35 @@
+package detail;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FindTheTownJudge {
+    public int findJudge(int N, int[][] trust) {
+        if (N == 1) {
+            return 1;
+        }
+        HashMap<Integer, Set<Integer>> trustOthers = new HashMap<>();
+        HashMap<Integer, Set<Integer>> trusted = new HashMap<>();
+        for (int i = 0; i < trust.length; i++) {
+            // 相信别人
+            if (!trustOthers.containsKey(trust[i][0])) {
+                trustOthers.put(trust[i][0], new HashSet<>());
+            }
+            trustOthers.get(trust[i][0]).add(trust[i][1]);
+
+            // 被别人相信
+            if (!trusted.containsKey(trust[i][1])) {
+                trusted.put(trust[i][1], new HashSet<>());
+            }
+            trusted.get(trust[i][1]).add(trust[i][0]);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!trustOthers.containsKey(i) && trusted.containsKey(i) && trusted.get(i).size() == N - 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}

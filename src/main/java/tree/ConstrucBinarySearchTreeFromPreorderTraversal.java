67467e7cae2ad2eb7dc6e0a017e389c015782eac
LiMingji <limingji0503@126.com>
2019-05-12 10:13:17 +0800

    1008. Construct Binary Search Tree from Preorder Traversal

---
+++ src/main/java/tree/ConstrucBinarySearchTreeFromPreorderTraversal.java
@@ -0,0 +1,29 @@
+package tree;
+
+import java.util.Arrays;
+
+public class ConstrucBinarySearchTreeFromPreorderTraversal {
+    public TreeNode generate(int[] preorder, int[] inorder, int preLeft, int preRight, int inLeft, int inRright) {
+        if (preLeft > preRight) {
+            return null;
+        }
+        int rootVal = preorder[preLeft];
+        TreeNode root = new TreeNode(rootVal);
+        int rootPos = inLeft;
+        for (int i = inLeft; i <= inRright; i++) {
+            if (inorder[i] == rootVal) {
+                rootPos = i;
+                break;
+            }
+        }
+        root.left = generate(preorder, inorder, preLeft + 1, preLeft + rootPos - inLeft, inLeft, rootPos - 1);
+        root.right = generate(preorder, inorder, preLeft + rootPos - inLeft + 1, preRight, rootPos + 1, inRright);
+        return root;
+    }
+
+    public TreeNode bstFromPreorder(int[] preorder) {
+        int[] inorder = Arrays.copyOf(preorder, preorder.length);
+        Arrays.sort(inorder);
+        return generate(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
+    }
+}

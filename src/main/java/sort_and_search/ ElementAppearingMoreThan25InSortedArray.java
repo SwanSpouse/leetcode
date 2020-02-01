1bdb34720800beac92f3ea4354d04b0fd8c05e62
李洺吉 <limingji@bytedance.com>
2020-02-01 15:37:35 +0800

    1287. Element Appearing More Than 25% In Sorted Array

--- src/main/java/sort_and_search/ ElementAppearingMoreThan25InSortedArray.java
+++ src/main/java/sort_and_search/ ElementAppearingMoreThan25InSortedArray.java
@@ -2,6 +2,20 @@ package sort_and_search;
 
 class  ElementAppearingMoreThan25InSortedArray {
     public int findSpecialInteger(int[] arr) {
-        return 0;
+        if(arr.length == 0) {
+            return 0;
+        }
+        int count = 1;
+        for(int i =1; i < arr.length; i ++) {
+            if(arr[i] == arr[i-1]) {
+                count += 1;
+                if(count > arr.length / 4) {
+                    return arr[i];
+                }
+            } else {
+                count = 1;
+            }
+        }
+        return arr[arr.length-1];
     }
 }

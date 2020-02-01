fca25cb5714432e9cf2cd35311145e6e38d83b58
李洺吉 <limingji@bytedance.com>
2020-02-01 18:42:16 +0800

    1302. Deepest Leaves Sum

---
+++ src/main/java/tree/DeepestLeavesSum.java
@@ -0,0 +1,30 @@
+package tree;
+
+class DeepestLeavesSum {
+    public int deepestLeavesSum(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        List<TreeNode> cur = new ArrayList<TreeNode>();
+        List<TreeNode> next = new ArrayList<TreeNode>();
+
+        next.add(root);
+        while(!next.isEmpty()){
+            cur = new ArrayList<TreeNode>(next);
+            next.clear();
+            for(TreeNode node : cur) {
+                if(node.left != null) {
+                    next.add(node.left);
+                }
+                if(node.right != null) {
+                    next.add(node.right);
+                }
+            }
+        }
+        int ret = 0;
+        for(TreeNode node : cur) {
+            ret += node.val;
+        }
+        return ret;
+    }
+}

226865cfa7adab8881194a74bab970857302dab3
李洺吉 <limingji@bytedance.com>
2020-02-02 15:37:53 +0800

    1329. Sort the Matrix Diagonally format codes

--- src/main/java/sort_and_search/SortTheMatrixDiagonally.java
+++ src/main/java/sort_and_search/SortTheMatrixDiagonally.java
@@ -7,31 +7,30 @@ import java.util.Collections;
 import java.util.List;
 
 public class SortTheMatrixDiagonally {
+
+
+    public void sort(int[][] mat, int i, int j) {
+        List<Integer> cur = new ArrayList<>();
+        for (int x = i, y = j; x < mat.length && y < mat[x].length; x++, y++) {
+            cur.add(mat[x][y]);
+        }
+        Collections.sort(cur);
+        for (int x = i, y = j, index = 0; x < mat.length && y < mat[x].length; x++, y++, index++) {
+            mat[x][y] = cur.get(index);
+        }
+    }
+
     public int[][] diagonalSort(int[][] mat) {
         if (mat.length == 0) {
             return mat;
         }
         // left
         for (int i = 0; i < mat.length; i++) {
-            List<Integer> cur = new ArrayList<>();
-            for (int x = i, y = 0; x < mat.length && y < mat[x].length; x++, y++) {
-                cur.add(mat[x][y]);
-            }
-            Collections.sort(cur);
-            for (int x = i, y = 0, index = 0; x < mat.length && y < mat[x].length; x++, y++, index++) {
-                mat[x][y] = cur.get(index);
-            }
+            sort(mat, i, 0);
         }
         // top
         for (int j = 1; j < mat[0].length; j++) {
-            List<Integer> cur = new ArrayList<>();
-            for (int y = j, x = 0; x < mat.length && y < mat[x].length; x++, y++) {
-                cur.add(mat[x][y]);
-            }
-            Collections.sort(cur);
-            for (int y = j, x = 0, index = 0; x < mat.length && y < mat[x].length; x++, y++, index++) {
-                mat[x][y] = cur.get(index);
-            }
+            sort(mat, 0, j);
         }
         return mat;
     }

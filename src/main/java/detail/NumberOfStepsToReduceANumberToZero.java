83038211750a9657ea131c640bc824d369f0a9de
李洺吉 <li_mingji@126.com>
2020-02-09 19:52:11 +0800

    1342. Number of Steps to Reduce a Number to Zero

---
+++ src/main/java/detail/NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,13 @@
+package detal;
+
+
+class  NumberOfStepsToReduceANumberToZero {
+    public int numberOfSteps (int num) {
+        int count = 0;
+        while(num != 0) {
+            num = num % 2 == 0 ? num / 2 : num - 1;
+            count += 1;
+        }
+        return count ;
+    }
+}

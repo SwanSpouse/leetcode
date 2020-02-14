3b3f2f047835f692c09c1e7a048dc8bfe1fcb6ae
李洺吉 <limingji@bytedance.com>
2020-02-14 11:57:12 +0800

    1332. Remove Palindromic Subsequences

--- src/main/java/strings/RemovePalindromicSubsequences.java
+++ src/main/java/strings/RemovePalindromicSubsequences.java
@@ -2,23 +2,7 @@ package strings;
 
 public class RemovePalindromicSubsequences {
 
-    public boolean isPalindrome(String s) {
-        for (int left = 0, right = s.length() - 1; left <= right; left++, right--) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public int removePalindromeSub(String s) {
-        if (s.isEmpty()) {
-            return 0;
-        } else if (isPalindrome(s)) {
-            return 1;
-        }
-        int step = 0;
-
-        return step;
+        return s.isEmpty() ? 0 : (s.equals(new StringBuilder(s).reverse().toString()) ? 1 : 2);
     }
 }

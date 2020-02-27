df98fede80086c3d90d2e192381df69d121fb03f
李洺吉 <limingji@bytedance.com>
2020-02-27 18:59:21 +0800

    wip 932. Beautiful Array

--- src/main/java/array/BeautifulArray.java
+++ src/main/java/array/BeautifulArray.java
@@ -1,4 +1,21 @@
 package array;
 
+import utils.Print;
+
 public class BeautifulArray {
+
+    public int[] beautifulArray(int N) {
+        int[] ret = new int[N];
+        for (int i = 1; i <= N; i += 2) {
+            ret[i - 1] = i + 1;
+            if (i < N) {
+                ret[i] = i;
+            }
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        Print.printArr(new BeautifulArray().beautifulArray(4));
+    }
 }
